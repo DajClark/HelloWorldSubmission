25f605919da115d603cf1842407dd6a200852791
Daniel Clark <dajclark@googlemail.com>
2018-12-23 20:34:52 +0000

    changed hello world message to xml strings value

--- app/src/main/java/com/example/helloworldsubmission/MainActivity.java
+++ app/src/main/java/com/example/helloworldsubmission/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Overrides the activities onCreate method responsible for creating the
+    // activity upon launch to provide custom functionality
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Calls the Activity super class code to run alongside custom code
         super.onCreate(savedInstanceState);
+
+        //Sets the view using the xml data contained within the layout folder
         setContentView(R.layout.activity_main);
     }
 }
